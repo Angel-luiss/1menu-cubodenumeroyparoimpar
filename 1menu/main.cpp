6a20592f1501701e4468c4fbc800235f132f5f41
Angel <Angel>
2022-06-14 23:20:51 -0600

    primer commit

--- 1menu/main.cpp
+++ 1menu/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main(){
+    int numero,op;
+    cout<<"Ingrese Opcion"<<endl;
+    cout<<"-------------------"<<endl;
+    cout<<"1. Cubo de numero"<<endl;
+    cout<<"2. Numero par o impar"<<endl;
+    cout<<"Salir"<<endl;
+    cout<<"Opcion: ";
+    cin>>op;
+    
+    switch (op) {
+        case 1:
+            cout<<"ingrese numero:"; cin>>numero;
+            
+            numero = (numero*3);
+            cout<<"el cubo del numero es:"<<numero<<endl;
+            break;
+        case 2:
+            cout<<"ingrese numero:"; cin>>numero;
+            
+            if (numero %2==0){
+                cout<<"el numero es par"<<endl;
+            }else{
+                cout<<"el numero es impar"<<endl;
+            }
+            break;
+            
+        case 3: break;
+            
+    }
     return 0;
 }
+
+
